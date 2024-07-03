94cfefa5cd36e96da7fd9509a4c9d5c9682ea879
Rushil Choudhary <choudharyrushil118@gmail.com>
2024-07-03 10:10:45 +0530

    post command

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 let courses = [
     {id: 1, name: "java"},
@@ -14,4 +15,21 @@ app.listen(port, () => {
 
 app.get('/courses', (req, res) => {
     res.json(courses);
+})
+
+app.post('/courses', (req, res) => {
+    
+    const receivedData = req.body;
+    console.log('Received data:', receivedData);
+
+    const newId = courses.length ? courses[courses.length - 1].id + 1 : 1;
+
+    const newCourse = {
+        id: newId,
+        name: receivedData.name
+    };
+
+    courses.push(newCourse);
+    
+    res.json({ message: 'Data received successfully', data: newCourse });
 })
