7047a5a97fbb3a3add6683bdfd31bd0fa802df28
Rushil Choudhary <choudharyrushil118@gmail.com>
2024-07-03 11:31:42 +0530

    reading from a courses file

--- index.js
+++ index.js
@@ -1,110 +1,90 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const dataFilePath = path.join(__dirname, 'course.json');
+
 app.use(express.json());
+app.use(logger);
+app.use(middleware);
 
-let courses = [
-    {id: 1, name: "java"},
-    {id: 2, name: "javascript"},
-    {id: 3, name: "python"}
-];
+function readCourses() {
+    try {
+        const data = fs.readFileSync(dataFilePath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading courses data:', error);
+        return [];
+    }
+}
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+function writeCourses(courses) {
+    try {
+        fs.writeFileSync(dataFilePath, JSON.stringify(courses, null, 2), 'utf8');
+    } catch (error) {
+        console.error('Error writing courses data:', error);
+    }
+}
 
+app.get('/courses', (req, res) => {
+    const courses = readCourses();
+    res.json(courses);
+});
 
-app.use(logger_middleware);
+app.post('/courses', (req, res) => {
+    const courses = readCourses();
+    let newCourse = { id: courses.length + 1, name: req.body.name };
+    courses.push(newCourse);
+    writeCourses(courses);
+    res.send(courses);
+});
 
-function logger_middleware(req, res, next){
-    //console.log method, ip, hostname, date
-    
+app.put('/courses/:id', (req, res) => {
+    try {
+        let courses = readCourses();
+        let singleCourse = courses.find(course => course.id === parseInt(req.params.id));
+        if (!singleCourse) {
+            res.status(404).send('Course does not exist');
+            return;
+        }
+        singleCourse.name = req.body.name;
+        writeCourses(courses);
+        res.send(singleCourse);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.delete('/courses', (req, res) => {
+    const courseId = parseInt(req.body.id);
+    let courses = readCourses();
+    const courseIndex = courses.findIndex(c => c.id === courseId);
+
+    if (courseIndex !== -1) {
+        courses.splice(courseIndex, 1);
+        writeCourses(courses);
+        res.json({ data: courses });
+    } else {
+        res.status(404).send('Course not found');
+    }
+});
+
+function middleware(req, res, next) {
+    console.log("called middleware");
+    next();
+}
+
+function logger(req, res, next) {
     const method = req.method;
     const ip = req.ip;
     const hostname = req.hostname;
     const date = new Date().toISOString();
-
-    console.log(`[${date}] ${method} request from ${hostname} (IP: ${ip})`);
-
-    next(); // Call the next middleware or route handler
-    
-}
-
-app.get('/courses', (req, res) => {
-    res.json(courses);
-})
-
-app.post('/courses', (req, res) => {
-    
-    const receivedData = req.body;
-    console.log('Received data:', receivedData);
-
-    const newId = courses.length ? courses[courses.length - 1].id + 1 : 1;
-
-    const newCourse = {
-        id: newId,
-        name: receivedData.name
-    };
-
-    courses.push(newCourse);
-    
-    res.json({data: courses });
-})
-
-app.put('/courses/:id', (req, res) => {
-    // const courseId = parseInt(req.body.id);
-    // const updatedData = req.body;
-
-    // const course = courses.find(c => c.id === courseId);
-    // course.name = updatedData.name || course.name;
-
-    // res.json({ data: course });
-
-    try{
-        let SingleCourse = courses.find((course) => {
-            return course.id === +req.params.id
-        })
-        if(!SingleCourse){
-            res.status(404).send('course not found');
-        }
-
-        SingleCourse.name = req.body.name;
-        res.send(courses);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-
-});
-
-app.delete('/courses/:id', (req, res) => {
-    // const courseId = parseInt(req.body.id);
-
-    // const courseIndex = courses.findIndex(c => c.id === courseId);
-
-    // courses.splice(courseIndex, 1);
-
-    // res.json({ data: courses });
-
-    try{
-        const courseId = parseInt(req.params.id);
-
-        const courseIndex = courses.findIndex(c => c.id === courseId);
-        if (courseIndex === -1) {
-            return res.status(404).json({ message: 'Course not found' });
-        }
-
-        courses.splice(courseIndex, 1);
-
-        res.json({ data: courses });
-
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-
-function middleware(req, res, next){
-    console.log("called");
+    console.log(`${method} request from ${ip} (hostname: ${hostname}) at Date: ${date}`);
     next();
 }
 
+const PORT = 2000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
